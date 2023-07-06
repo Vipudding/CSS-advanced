1a70213123762098c1cf6f7ae72b863702d00915
Vipudding <98952502+Vipudding@users.noreply.github.com>
2023-07-06 18:17:44 -0300

    06/07/

--- script.js
+++ script.js
@@ -22,7 +22,39 @@ window.addEventListener("scroll", function() {
       }
     }
   })
-  
+// Função para pesquisar os dados da API com base na entrada do usuário
+function searchSWAPI(searchTerm) {
+  const searchUrl = `https://swapi.dev/api/?search=${searchTerm}`;
+
+  fetchData(searchUrl)
+    .then(data => displaySearchResults(data.results))
+    .catch(error => console.log(error));
+}
+
+// Função para exibir os resultados da pesquisa
+function displaySearchResults(results) {
+  const searchResultsContainer = document.getElementById('search-results');
+  searchResultsContainer.innerHTML = '';
+
+  results.forEach(result => {
+    const resultElement = document.createElement('p');
+    resultElement.textContent = result.name || result.title;
+    searchResultsContainer.appendChild(resultElement);
+  });
+}
+
+// Função para manipular o evento de clique no botão de pesquisa
+function handleSearch() {
+  const searchInput = document.getElementById('search-input');
+  const searchTerm = searchInput.value;
+
+  searchSWAPI(searchTerm);
+}
+
+// Adicionar ouvinte de evento para o clique no botão de pesquisa
+const searchButton = document.getElementById('search-button');
+searchButton.addEventListener('click', handleSearch);
+
   
   
   
